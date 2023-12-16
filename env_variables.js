851a34198c971098bf4a0b07e1a6274f21e62022
ezra-87 <147218464+ezra-87@users.noreply.github.com>
2023-12-16 12:44:47 +0800

    Add files via upload

---
+++ env_variables.js
@@ -0,0 +1,7 @@
+// env_variables.js
+const envVariables = {
+    BING_MAPS_API_KEY: 'AgkWMZlk5ts6xb8cJkzUar2iJMWTexduafRzsyANqeAF2b_PN0D2CZAKo8hfNqkB',
+    MONGODB_URI: 'mongodb+srv://itsupport:GSB110011@cluster0.kkzdiku.mongodb.net/LMS?retryWrites=true&w=majority',
+};
+
+module.exports = envVariables;
